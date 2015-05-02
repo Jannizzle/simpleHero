ab83180cecfc62d06df18a0cd01f2dd8f3fe3869
Joannis Xenakis <jannis.xenakis@gmx.de>
2015-05-02 23:29:02 +0200

    v0.2.5
    added loading bar

--- Assets/Scripts/Menu/LoadingScreen.cs
+++ Assets/Scripts/Menu/LoadingScreen.cs
@@ -6,12 +6,36 @@ public class LoadingScreen : MonoBehaviour
 {
 
 
-	public Text bar;
-
+	public Text text;
+	public Image bar;
+	float originalScale;
+	int tmp;
 	// Use this for initialization
 	void Start ()
 	{
-		Application.LoadLevel(1);
+		originalScale = bar.rectTransform.localScale.x;
+		//Application.LoadLevel(1);
+		StartCoroutine (load ());
+	
+	}
+
+	void Update ()
+	{
+		//bar.text = ""+tmp;
+	}
+
+	IEnumerator load ()
+	{
+
+		AsyncOperation async = Application.LoadLevelAsync (1);
+		while (!async.isDone) {
+			text.text = "Progress " + (int)(async.progress * 100) + "%";
+			bar.rectTransform.localScale = new Vector3 (async.progress * originalScale, bar.rectTransform.localScale.y, 1);
+
+			yield return null;
+		}
+
+
 	}
 
 }
